700d7bcac69ce61d2ec371f67e5fdbcdbdd77cf7
nobus <meganobus@gmail.com>
2019-12-03 10:16:24 +0300

    create vsminer django project

---
+++ vs_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VsAppConfig(AppConfig):
+    name = 'vs_app'
---
+++ vsminer/settings_dev.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vsminer',
+        'USER': 'vsminer',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
