0bb9e62896eb251cfd17e9b1bc77e76ccea8a135
nobus <meganobus@gmail.com>
2019-12-06 13:26:00 +0300

    Implementations of method get_job_info

--- vs_app/management/commands/astrometry.py
+++ vs_app/management/commands/astrometry.py
@@ -4,12 +4,26 @@ import sys
 import json
 import requests
 
+import shutil
+
+import msgpack
+import msgpack_numpy as m
+m.patch()
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from astropy.io import fits
+from astropy.utils.data import download_file
+
 from django.core.management.base import BaseCommand
+
+from vs_app.models import AstroMetryJob
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('--job-id', dest='job_id', type=int, help='job id')
+        parser.add_argument('--job-number', dest='job_number', type=int, help='job number')
         parser.add_argument('--job-info', dest='job_info', help='get job info', action='store_true')
         parser.add_argument('--new-image', dest='new_image', help='get new_image.fits', action='store_true')
         parser.add_argument('--corr-image', dest='corr', help='get corr.fits', action='store_true')
@@ -20,14 +34,30 @@ class Command(BaseCommand):
         http://astrometry.net/doc/net/api.html#getting-job-results
         """
 
-        print(f'Get job info for job={self.job_id}')
-        resp = requests.get(f'http://nova.astrometry.net/api/jobs/{self.job_id}/info/')
+        print(f'Get job info for job={self.job_number}')
+        resp = requests.get(f'http://nova.astrometry.net/api/jobs/{self.job_number}/info/')
 
         job_info = json.loads(resp.text)
         job_stat = job_info.get('status', '')
 
         if job_stat == 'success':
-            print(job_info)
+            calibration = job_info.get('calibration', {})
+
+            obj, created = AstroMetryJob.objects.get_or_create(
+                job_number=self.job_number,
+                center_ra=calibration['ra'],
+                center_dec=calibration['dec'],
+            )
+
+            obj.orientation = calibration.get('orientation', None)
+            obj.pixscale = calibration.get('pixscale', None)
+            obj.radius = calibration.get('radius', None)
+
+            if not created:
+                obj.center_ra = calibration['ra']
+                obj.center_dec = calibration['dec']
+
+            obj.save()
         else:
             raise BaseException(f'Job {self.job_id} has wrong status: {job_stat}')
 
@@ -43,7 +73,7 @@ class Command(BaseCommand):
         self.get_corr()
 
     def handle(self, *args, **options):
-        self.job_id = options['job_id']
+        self.job_number = options['job_number']
 
         if options['all']:
             self.get_all()
