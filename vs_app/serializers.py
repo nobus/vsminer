cfebacbf08bafa5a4734c536e5fb45b8650dee9c
nobus <meganobus@gmail.com>
2019-12-10 17:38:53 +0300

    Add SimbadDataSerializer

--- vs_app/serializers.py
+++ vs_app/serializers.py
@@ -2,7 +2,7 @@
 
 from rest_framework import serializers
 
-from vs_app.models import AstroMetryJob, CorrFits, AAVSOData
+from vs_app.models import AstroMetryJob, CorrFits, AAVSOData, SimbadData
 
 
 class AstroMetryJobSerializer(serializers.ModelSerializer):
@@ -13,12 +13,18 @@ class AstroMetryJobSerializer(serializers.ModelSerializer):
 class AAVSODataSerializer(serializers.ModelSerializer):
     class Meta:
         model = AAVSOData
-        fields = ['Name', 'AUID', 'VariabilityType', 'Period', 'MaxMag', 'MinMag']
+        fields = ['Name', 'AUID', 'VariabilityType', 'Period', 'MaxMag', 'MinMag', 'Category']
+
+class SimbadDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SimbadData
+        fields = ['MAIN_ID']
 
 class CorrFitsSerializer(serializers.ModelSerializer):
     aavso_data = AAVSODataSerializer(many=False, read_only=True)
+    simbad_data = SimbadDataSerializer(many=False, read_only=True)
     astro_job = AstroMetryJobSerializer(many=False, read_only=True)
 
     class Meta:
         model = CorrFits
-        fields = ['field_x', 'field_y', 'field_ra', 'field_dec', 'aavso_data', 'astro_job']
+        fields = ['field_x', 'field_y', 'field_ra', 'field_dec', 'aavso_data', 'simbad_data', 'astro_job']
