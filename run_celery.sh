1d75e87037a9678def99b68e1eda00e5888c3f03
nobus <meganobus@gmail.com>
2019-12-12 13:58:11 +0300

    - add DJANGO_SETTINGS_MODULE - impruvement examples

--- run_celery.sh
+++ run_celery.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 : '
+ sudo rabbitmqctl list_queues
+ ...
  workon vsminer
  python manage.py shell
+
  from vs_app import tasks
+ from celery.task.control import inspect
+
  result = tasks.sleep.delay(120)
+ i = inspect()
+ i.active()
 '
+export DJANGO_SETTINGS_MODULE=vsminer.settings
 
 celery -A vs_app worker --loglevel=debug
--- run_flower.sh
+++ run_flower.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export DJANGO_SETTINGS_MODULE=vsminer.settings
+
 celery flower -A vs_app --address=127.0.0.1 --port=5555
