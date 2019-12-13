ebf97a2ba0472db19b822b72457c4067e71e6ab5
nobus <meganobus@gmail.com>
2019-12-13 16:27:57 +0300

    Add views PutAstrometryJob and CeleryStatus

--- vs_app/urls.py
+++ vs_app/urls.py
@@ -3,12 +3,16 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from vs_app.views import AstroMetryJobViewSet, CorrFitsViewSet
+from vs_app.views import (
+    AstroMetryJobViewSet, CorrFitsViewSet, CeleryStatus, PutAstrometryJob)
 
 router = DefaultRouter()
 router.register(r'jobs', AstroMetryJobViewSet)
 router.register(r'star_data', CorrFitsViewSet)
 
 urlpatterns = [
+    path('put_astrojob/', PutAstrometryJob.as_view()),
+    path('celery_status/', CeleryStatus.as_view()),
+
     path('', include(router.urls)),
 ]
--- vs_app/views.py
+++ vs_app/views.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+import json
 
-from rest_framework.decorators import api_view
+from django.views import View
+from django.http import JsonResponse
+
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 
+from celery.task.control import inspect
+
+from vs_app import tasks
 from vs_app.models import AstroMetryJob, CorrFits
 from vs_app.serializers import AstroMetryJobSerializer, CorrFitsSerializer
 
@@ -20,3 +25,60 @@ class CorrFitsViewSet(viewsets.ReadOnlyModelViewSet):
 
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('astro_job',)
+
+
+class CeleryStatus(View):
+    def get(self, request, *args, **kwargs):
+        """
+        {
+            "active": {
+                "celery@nobus": [
+                    {
+                        "id": "19ce8e20-0d6a-4892-8e81-a73bb473982a",
+                        "name": "vs_app.tasks.sleep",
+                        "args": "(120,)",
+                        "kwargs": "{}",
+                        "type": "vs_app.tasks.sleep",
+                        "hostname": "celery@nobus",
+                        "time_start": 1576218943.5906646,
+                        "acknowledged": true,
+                        "delivery_info": {
+                            "exchange": "",
+                            "routing_key": "celery",
+                            "priority": 0,
+                            "redelivered": false
+                        }, "worker_pid": 17513
+                    }
+                ]},
+            "sheduled": {"celery@nobus": []}}
+        """
+        resp = []
+
+        i = inspect()
+        active = i.active()
+
+        if active:
+            for task_list in active.values():
+                for task in task_list:
+                    resp.append({
+                        'id': task['id'],
+                        'name': task['name'],
+                        'args': task.get('args', ''),
+                        'kwargs': task.get('kwargs', ''),
+                        })
+
+        return JsonResponse({"resp": resp})
+
+class PutAstrometryJob(View):
+    def post(self, request, *args, **kwargs):
+        data = request.read()
+        params = json.loads(data.decode('ascii'))
+
+        print(params)
+
+        tasks.download_astrometry_data.delay(
+            int(params['job_number']),
+            params['status_url'],
+        )
+
+        return JsonResponse({'status': 'ok'})
