714db7043148ceeecef53839dc3a8d485363b88e
nobus <meganobus@gmail.com>
2020-01-22 16:29:49 +0300

    refactoring

--- downloader.sh
+++ downloader.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# IRIS 5.59, Canon EOS 1300D, Jupiter 37A, ISO=400, exp=30''
+# RAWTRAN 0.3.8, Munipack 0.5.10, IRIS 5.59, Canon EOS 1300D, Jupiter 37A, ISO=400, exp=30''
 
-# 3850026   'http://nova.astrometry.net/user_images/3302553#annotated'
-# 3850029   'http://nova.astrometry.net/user_images/3302556#annotated'
-# 3850033   'http://nova.astrometry.net/user_images/3302560#annotated'
-
-JOB_NUMBER=3850033
-STATUS_URL='http://nova.astrometry.net/user_images/3302560#annotated'
+JOB_NUMBER=$1
+STATUS_URL=$2
 
 python manage.py astrometry --job-number $JOB_NUMBER --status-url $STATUS_URL --all
 python manage.py simbad --job-number $JOB_NUMBER
